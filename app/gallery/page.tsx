9ceda357643c75fd8b2629d081e214eb7a218c3a
DIVIJ08070 <divijpatel.co22d2@scet.ac.in>
2025-03-28 21:05:49 +0530

    final committvideobeauty3

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -5,16 +5,16 @@ import Image from "next/image";
 import { Footer } from '../comonents/footer';
 
 const data = [
-    { id: 1, image: 'https://picsum.photos/id/10/200/300', height: 600 },
-    { id: 2, image: 'https://picsum.photos/id/14/200/300', height: 400 },
-    { id: 3, image: 'https://picsum.photos/id/15/200/300', height: 400 },
-    { id: 4, image: 'https://picsum.photos/id/16/200/300', height: 400 },
-    { id: 5, image: 'https://picsum.photos/id/17/200/300', height: 400 },
-    { id: 6, image: 'https://picsum.photos/id/19/200/300', height: 400 },
-    { id: 7, image: 'https://picsum.photos/id/37/200/300', height: 300 },
-    { id: 8, image: 'https://picsum.photos/id/39/200/300', height: 400 },
-    { id: 9, image: 'https://picsum.photos/id/85/200/300', height: 300 },
-    { id: 10, image: 'https://picsum.photos/id/103/200/300', height: 600 }
+    { id: 1, image: '/image4.jpg', height: 600 },
+    { id: 3, image: '/image3.jpg', height: 400 },
+    { id: 4, image: '/image6.jpg', height: 400 },
+    { id: 2, image: '/image2.jpeg', height: 400 },
+    { id: 5, image: '/image1.jpeg', height: 400 },
+    { id: 6, image: '/image5.jpg', height: 400 },
+    { id: 7, image: '/image4.jpg', height: 300 },
+    { id: 8, image: '/image3.jpg', height: 400 },
+    { id: 9, image: '/image6.jpg', height: 300 },
+    { id: 10, image: '/image2.jpeg', height: 600 }
 ];
 
 const Gallery = () => {
