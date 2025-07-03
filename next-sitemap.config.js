06b602efbf8065e1dcfa0908c632587cb5f799a0
krish-3011 <krishpatel.co22d2@scet.ac.in>
2025-07-03 15:17:40 +0530

    sitemap seo

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+// next-sitemap.config.js
+module.exports = {
+  siteUrl: 'https://chhayapartyplot.com',
+  generateRobotsTxt: true,
+  changefreq: 'daily',
+  priority: 0.7,
+  sitemapSize: 5000,
+};
