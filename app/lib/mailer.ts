94932aa1047729054f358325490c353332e1a941
Mann Patel <mann.patel.1207@gmail.com>
2025-07-08 00:13:53 +0530

    Zoho mailer added

--- app/lib/mailer.ts
+++ app/lib/mailer.ts
@@ -1,7 +1,7 @@
 import nodemailer from 'nodemailer';
 
 export const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.zoho.in',
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
